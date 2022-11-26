ecfadc3e25654fc70bcaeef7ad15b1364f6c4eff
Peikrishvili <ypeikris@its.jnj.com>
2022-11-26 23:24:10 +0300

    dz done

--- src/app/components/user.jsx
+++ src/app/components/user.jsx
@@ -1,29 +1,51 @@
 import React from "react";
 import Qualitie from "./qualitie";
 import Bookmark from "./bookmark";
+import PropTypes from "prop-types";
 
-    const User = (user) => {
-
+const User = (user) => {
     return (
         <tr>
             <td>{user.name}</td>
             <td>
                 {user.qualities.map((qualitie, index) => (
-                    <Qualitie  key = {index} color = {qualitie.color} name = {qualitie.name}/>
+                    <Qualitie
+                        key={index}
+                        color={qualitie.color}
+                        name={qualitie.name}
+                    />
                 ))}
             </td>
             <td>{user.profession.name}</td>
             <td>{user.completedMeetings}</td>
             <td>{user.rate}</td>
             <td>
-                <Bookmark status = {user.bookmark} onToogleBookmark = {user.onToogleBookmark} id={user._id}/> 
+                <Bookmark
+                    status={user.bookmark}
+                    onToogleBookmark={user.onToogleBookmark}
+                    id={user._id}
+                />
             </td>
             <td>
-                <button className="btn btn-danger" onClick={() => user.onDelete(user._id)}>delete</button>
+                <button
+                    className="btn btn-danger"
+                    onClick={() => user.onDelete(user._id)}
+                >
+                    delete
+                </button>
             </td>
         </tr>
-        
-    )
-}
+    );
+};
+User.protoTypes = {
+    _id: PropTypes.string.isRequired,
+    users: PropTypes.array.isRequired,
+    bookmark: PropTypes.bool.isRequired,
+    onDelete: PropTypes.func.isRequired,
+    onToogleBookmark: PropTypes.func.isRequired,
+    profession: PropTypes.object.isRequired,
+    completedMeetings: PropTypes.number.isRequired,
+    rate: PropTypes.number.isRequired
+};
 
 export default User;
